59f4ec6d9d375802d679558534ef1b625a1ee633
Yury Antonov <yantonov@yandex.ru>
2015-02-03 22:58:21 +0500

    +kibit

--- profiles.clj
+++ profiles.clj
@@ -2,6 +2,7 @@
                        [pjstadig/humane-test-output "0.6.0"]]
         :plugins [[cider/cider-nrepl "0.8.2"]
                   [lein-cljfmt "0.1.4"]
+                  [lein-kibit "0.0.8"]
                   [refactor-nrepl "0.2.2"]
                   [lein-ancient "0.6.1"]
                   [jonase/eastwood "0.2.1"]
