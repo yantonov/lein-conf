666971c93324e4c21711a0a5ee277b6d7099a1b7
Yury Antonov <yantonov@yandex.ru>
2015-12-13 20:54:01 +0500

    bump up lein ancient

--- profiles.clj
+++ profiles.clj
@@ -5,7 +5,7 @@
                   [lein-cljfmt "0.3.0"]
                   [lein-kibit "0.1.2"]
                   [refactor-nrepl "1.1.0"]
-                  [lein-ancient "0.6.7"]
+                  [lein-ancient "0.6.8"]
                   [jonase/eastwood "0.2.1"]
                   [venantius/ultra "0.3.4"]]
         :ultra {:color-scheme :solarized_dark}
