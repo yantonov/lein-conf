37bf684ad8419a2171a48090df85335093505895
Yury Antonov <yantonov@yandex.ru>
2015-12-13 20:56:06 +0500

    automatically bump up plugins versions

--- profiles.clj
+++ profiles.clj
@@ -1,13 +1,13 @@
 {:user {:dependencies [[alembic "0.3.2"]
-                       [pjstadig/humane-test-output "0.7.0"]
-                       [org.clojure/tools.nrepl "0.2.10"]]
-        :plugins [[cider/cider-nrepl "0.9.1"]
+                       [pjstadig/humane-test-output "0.7.1"]
+                       [org.clojure/tools.nrepl "0.2.12"]]
+        :plugins [[cider/cider-nrepl "0.10.0"]
                   [lein-cljfmt "0.3.0"]
                   [lein-kibit "0.1.2"]
-                  [refactor-nrepl "1.1.0"]
+                  [refactor-nrepl "1.2.0"]
                   [lein-ancient "0.6.8"]
-                  [jonase/eastwood "0.2.1"]
-                  [venantius/ultra "0.3.4"]]
+                  [jonase/eastwood "0.2.2"]
+                  [venantius/ultra "0.4.0"]]
         :ultra {:color-scheme :solarized_dark}
         :injections [(use 'clojure.repl)
                      (use 'clojure.java.javadoc)
