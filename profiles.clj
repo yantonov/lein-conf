cdd724c06003dd98c0e4d4b69c1893896ef39285
Yury Antonov <yantonov@yandex.ru>
2016-01-06 19:18:52 +0500

    move repl specific to corresponding profile

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,7 @@
 {:user
- {:dependencies [[alembic "0.3.2"]
-                 [pjstadig/humane-test-output "0.7.0"]
-                 [org.clojure/tools.nrepl "0.2.10"]]
+ {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
   :plugins [[lein-cljfmt "0.3.0"]
             [lein-kibit "0.1.2"]
-            [refactor-nrepl "1.1.0"]
             [lein-ancient "0.6.7"]
             [jonase/eastwood "0.2.1"]
             [venantius/ultra "0.3.4"]]
@@ -15,4 +12,10 @@
                (require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]}
  :repl
- {:plugins [[cider/cider-nrepl "0.10.1"]]}}
+ {:dependencies [[alembic "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.12"]]
+  :plugins [[cider/cider-nrepl "0.10.1"]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]
+  :injections [(use 'clojure.repl)
+               (use 'clojure.java.javadoc)
+               (require 'alembic.still)]}}
