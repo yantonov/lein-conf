3fff1a4b0ae5621aebe657d327fdd13de1c58cab
Yury Antonov <yantonov@yandex.ru>
2016-01-17 15:48:34 +0500

    use latest clj-refactor from melpa and nrepl 2.0.0-snapshot

--- profiles.clj
+++ profiles.clj
@@ -13,7 +13,7 @@
  :repl
  {:dependencies [[alembic "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[refactor-nrepl "1.1.0"]
+  :plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
             [cider/cider-nrepl "0.10.1"]]
   :injections [(use 'clojure.repl)
                (use 'clojure.java.javadoc)
