1ca5748ccf44e6b04cdf4f8af3f1c4f332ecc7f5
Yury Antonov <yantonov@yandex.ru>
2016-02-10 10:03:43 +0500

    actually usefull for test output

--- profiles.clj
+++ profiles.clj
@@ -2,7 +2,8 @@
  {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
   :plugins [[lein-cljfmt "0.3.0"]
             [lein-kibit "0.1.2"]
-            [lein-ancient "0.6.8"]]
+            [lein-ancient "0.6.8"]
+            [venantius/ultra "0.4.0"]]
   :injections [(use 'clojure.repl)
                (use 'clojure.java.javadoc)
                (require 'pjstadig.humane-test-output)
