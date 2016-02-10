0a9733f7bc7509a96de4bf786b60565436e7d2b2
Yury Antonov <yantonov@yandex.ru>
2016-02-10 09:24:34 +0500

    update to latest refactor nrepl

--- profiles.clj
+++ profiles.clj
@@ -12,7 +12,7 @@
  :repl
  {:dependencies [[alembic "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
+  :plugins [[refactor-nrepl "2.2.0-SNAPSHOT"]
             [cider/cider-nrepl "0.10.2"]]
   :injections [(use 'clojure.repl)
                (use 'clojure.java.javadoc)
