44a8a9024563b69245c92545fe49ebc6d7f9aaa2
Yury Antonov <yantonov@yandex.ru>
2016-04-09 09:11:28 +0500

    bump up artifact versions

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,8 @@
 {:user
- {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
-  :plugins [[lein-cljfmt "0.4.1"]
+ {:dependencies [[pjstadig/humane-test-output "0.8.0"]]
+  :plugins [[lein-cljfmt "0.5.1"]
             [lein-kibit "0.1.2"]
-            [lein-ancient "0.6.8"]
+            [lein-ancient "0.6.10"]
             [venantius/ultra "0.4.1"]]
   :injections [(use 'clojure.repl)
                (use 'clojure.java.javadoc)
@@ -11,8 +11,8 @@
  :repl
  {:dependencies [[alembic "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[refactor-nrepl "2.2.0-SNAPSHOT"]
-            [cider/cider-nrepl "0.10.2"]]
+  :plugins [[refactor-nrepl "2.2.0"]
+            [cider/cider-nrepl "0.11.0"]]
   :injections [(use 'clojure.repl)
                (use 'clojure.java.javadoc)
                (require 'alembic.still)]}}
