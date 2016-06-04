a5fed1030e9913d249089237968ae58f4fcb8458
Yury Antonov <yantonov@yandex.ru>
2016-06-04 21:13:17 +0500

    update linters

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,13 @@
 {:user
  {:dependencies [[pjstadig/humane-test-output "0.8.0"]]
   :plugins [[lein-cljfmt "0.5.1"]
-            [lein-kibit "0.1.2"]
             [lein-ancient "0.6.10"]
-            [venantius/ultra "0.4.1"]]
+            [venantius/ultra "0.4.1"]
+            ;; linters
+            [lein-kibit "0.1.2"]
+            [jonase/eastwood "0.2.3"]
+            [venantius/yagni "0.1.4"]
+            [lein-bikeshed "0.3.0"]]
   :injections [(use 'clojure.repl)
                (use 'clojure.java.javadoc)
                (require 'pjstadig.humane-test-output)
