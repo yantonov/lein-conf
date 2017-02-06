ce8ad2f8aed91ed1440c653fe6090de7b0d195bc
yantonov <yantonov@yandex.ru>
2017-02-06 11:33:30 +0500

    bump up ultra version

--- profiles.clj
+++ profiles.clj
@@ -2,7 +2,7 @@
  {:dependencies [[pjstadig/humane-test-output "0.8.1"]]
   :plugins [[lein-cljfmt "0.5.6"] ; reformat
             [lein-ancient "0.6.10"] ; check out-of-date artifact
-            [venantius/ultra "0.5.0"] ; pretty print test output, stacktraces
+            [venantius/ultra "0.5.1"] ; pretty print test output, stacktraces
             [lein-kibit "0.1.3"] ; lint
             [jonase/eastwood "0.2.3"] ; lint
             [venantius/yagni "0.1.4"] ; check dead code
