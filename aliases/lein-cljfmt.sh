bb1001982765a9db9eadd1a01534662a255e2094
Yury Antonov <yantonov@yandex.ru>
2017-02-23 14:57:47 +0500

    remove additional script

--- aliases/lein-cljfmt.sh
+++ aliases/lein-cljfmt.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-lein cljfmt check
+SCRIPT_NAME=`basename $0`
+
+if [ "$#" -gt 0 ]; then
+    CMD=$1
+else
+    CMD="check"
+fi
+
+lein cljfmt $CMD
+
+if [ "$#" -eq 0 ]; then
+    echo "if you want to apply changes use: $SCRIPT_NAME fix"
+fi
