f991e147f5b0cf22152fb6705ef6df0e531c4a82
Yury Antonov <yantonov@yandex.ru>
2017-02-23 15:05:52 +0500

    one responsibility - one script

--- aliases/lein-ancient.sh
+++ aliases/lein-ancient.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-lein ancient
+SCRIPT_NAME=`basename $0`
+
+if [ "$1" == "upgrade" ];
+then
+    lein ancient upgrade :interactive
+else
+    lein ancient
+    echo "if you want to apply changes, use: $SCRIPT_NAME upgrade"
+fi
