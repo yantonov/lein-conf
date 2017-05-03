31e47e657877ce67d6d6395c0f3ac3aca761ac84
Yury Antonov <yantonov@yandex.ru>
2017-05-03 17:14:02 +0500

    upgrade clojure

--- aliases/lein-ancient.sh
+++ aliases/lein-ancient.sh
@@ -4,9 +4,16 @@ SCRIPT_NAME=`basename $0`
 
 if [ "$1" = "upgrade" ]; then
     lein ancient upgrade :interactive
-else
-    lein ancient
-    if [ $? -ne 0 ]; then
-        echo "if you want to apply changes, use: $SCRIPT_NAME upgrade"
-    fi
+    exit 0
+fi
+
+if [ "$1" = "upgrade-clojure" ]; then
+    lein ancient :upgrade :check-clojure :interactive
+    exit 0
+fi
+
+lein ancient
+if [ $? -ne 0 ]; then
+    echo "if you want to apply changes, use: $SCRIPT_NAME upgrade"
+    echo "to upgrade clojure  use: $SCRIPT_NAME upgrade-clojure"
 fi
