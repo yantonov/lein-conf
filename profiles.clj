9a54b35b4e784556100e2089aa606d518d16848d
Yury Antonov <yantonov@yandex.ru>
2018-11-05 12:43:14 +0500

    bumped up artifact versions

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,11 @@
 {:user
- {:plugins [[lein-cljfmt "0.5.7"] ; reformat
-            [lein-ancient "0.6.14"] ; check out-of-date artifact
+ {:plugins [[lein-cljfmt "0.6.1"] ; reformat
+            [lein-ancient "0.6.15"] ; check out-of-date artifact
             [venantius/ultra "0.5.2"] ; pretty print test output, stacktraces
             [lein-kibit "0.1.6"] ; lint
-            [jonase/eastwood "0.2.5"] ; lint
-            [venantius/yagni "0.1.4"] ; check dead code
-            [lein-bikeshed "0.5.0"] ; another lint tool
+            [jonase/eastwood "0.3.3"] ; lint
+            [venantius/yagni "0.1.6"] ; check dead code
+            [lein-bikeshed "0.5.1"] ; another lint tool
             [lein-auto "0.1.3"] ; watches the project source directories, and executes a task when it detects changes to files matching a set pattern
             ]
   }
@@ -14,8 +14,8 @@
                  [alembic "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  ]
-  :plugins [[refactor-nrepl "2.3.1"]
-            [cider/cider-nrepl "0.15.1"]]
+  :plugins [[refactor-nrepl "2.4.0"]
+            [cider/cider-nrepl "0.18.0"]]
   :injections [(require '[clojure.tools.namespace.repl :refer [refresh]])
                (refresh)
                (require 'alembic.still)
