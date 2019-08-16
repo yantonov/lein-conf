14901622fd5c175348b83039a2e0d51a3ac287bc
Yury Antonov <yantonov@yandex.ru>
2019-08-16 21:06:40 +0500

    bump up plugin versions

--- profiles.clj
+++ profiles.clj
@@ -1,21 +1,21 @@
 {:user
- {:plugins [[lein-cljfmt "0.6.1"] ; reformat
+ {:plugins [[lein-cljfmt "0.6.4"] ; reformat
             [lein-ancient "0.6.15"] ; check out-of-date artifact
-            [venantius/ultra "0.5.2"] ; pretty print test output, stacktraces
-            [lein-kibit "0.1.6"] ; lint
-            [jonase/eastwood "0.3.3"] ; lint
-            [venantius/yagni "0.1.6"] ; check dead code
-            [lein-bikeshed "0.5.1"] ; another lint tool
+            [venantius/ultra "0.6.0"] ; pretty print test output, stacktraces
+            [lein-kibit "0.1.7"] ; lint
+            [jonase/eastwood "0.3.6"] ; lint
+            [venantius/yagni "0.1.7"] ; check dead code
+            [lein-bikeshed "0.5.2"] ; another lint tool
             [lein-auto "0.1.3"] ; watches the project source directories, and executes a task when it detects changes to files matching a set pattern
             ]
   }
  :repl
- {:dependencies [[org.clojure/tools.namespace "0.2.11"] ; reload ns
+ {:dependencies [[org.clojure/tools.namespace "0.3.1"] ; reload ns
                  [alembic "0.3.2"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  ]
   :plugins [[refactor-nrepl "2.4.0"]
-            [cider/cider-nrepl "0.18.0"]]
+            [cider/cider-nrepl "0.21.1"]]
   :injections [(require '[clojure.tools.namespace.repl :refer [refresh]])
                (refresh)
                (require 'alembic.still)
