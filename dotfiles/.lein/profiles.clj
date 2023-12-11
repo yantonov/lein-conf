d50b8469f905aa8b560b3ec3c190e34ece335e40
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2023-12-11 22:39:44 +0100

    update plugins

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,14 +1,14 @@
 {:user
  {
-  :plugins [[lein-cljfmt "0.9.0"] ; reformat
+  :plugins [[lein-cljfmt "0.9.2"] ; reformat
             [lein-ancient "0.7.0"] ; check out-of-date artifact
             [lein-kibit "0.1.8"] ; lint
-            [jonase/eastwood "1.2.5"] ; lint
+            [jonase/eastwood "1.4.2"] ; lint
             [venantius/yagni "0.1.7"] ; check dead code
             [lein-bikeshed "0.5.2"] ; another lint tool
             [lein-auto "0.1.3"] ; watches the project source directories, and executes a task when it detects changes to files matching a set pattern
             ]
-  :dependencies [[hashp "0.2.1"]] ; https://github.com/weavejester/hashp
+  :dependencies [[hashp "0.2.2"]] ; https://github.com/weavejester/hashp
   :injections [(require 'hashp.core)]
   }
  :repl
