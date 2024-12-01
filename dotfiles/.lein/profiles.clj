8db7beeac5fe100e80be062bed806017af80c04c
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2024-12-01 07:03:38 +0100

    update dependencies

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -2,8 +2,8 @@
  {
   :plugins [[lein-cljfmt "0.9.2"] ; reformat
             [lein-ancient "0.7.0"] ; check out-of-date artifact
-            [lein-kibit "0.1.8"] ; lint
-            [jonase/eastwood "1.4.2"] ; lint
+            [lein-kibit "0.1.11"] ; lint
+            [jonase/eastwood "1.4.3"] ; lint
             [venantius/yagni "0.1.7"] ; check dead code
             [lein-bikeshed "0.5.2"] ; another lint tool
             [lein-auto "0.1.3"] ; watches the project source directories, and executes a task when it detects changes to files matching a set pattern
